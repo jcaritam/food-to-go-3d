f1808db404b07fbd3ef88ad5c260202e75919d47
Joseph Antonio Carita <joseph.cmvd@gmail.com>
2025-10-11 16:37:42 -0500

    feat(container-counter): Implementation of kitchen object container, get and drop objects on clean counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,48 +1,36 @@
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour, IKitchenObjectParent
+public class ClearCounter : BaseCounter
 {
       [SerializeField] private KitchenObjectsSO kitchenObjectsSO;
-      [SerializeField] private Transform counterTopPoint;
 
-      private KitchenObject kitchenObject;
 
-      public void Interact(Player player)
+      public override void Interact(Player player)
       {
-            if (kitchenObject == null)
+            if (!HasKitchenObject())
             {
-                  Transform kitchenObjectTransform = Instantiate(kitchenObjectsSO.prefab, counterTopPoint);
-                  kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+                  if (player.HasKitchenObject())
+                  {
+                        player.GetKitchenObject().SetKitchenObjectParent(this);
+                  }
+                  else
+                  {
+
+                  }
             }
             else
             {
-                  kitchenObject.SetKitchenObjectParent(player);
+                  if (player.HasKitchenObject())
+                  {
+
+                  }
+                  else
+                  {
+                        GetKitchenObject().SetKitchenObjectParent(player);
+                  }
             }
       }
 
-      public Transform GetKitchenObjectFollowTransform()
-      {
-            return counterTopPoint;
-      }
 
-      public void SetKitchenObject(KitchenObject kitchenObject)
-      {
-            this.kitchenObject = kitchenObject;
-      }
 
-      public KitchenObject GetKitchenObject()
-      {
-            return kitchenObject;
-      }
-
-      public void ClearKitchenObject()
-      {
-            kitchenObject = null;
-      }
-
-      public bool HasKitchenObject()
-      {
-            return kitchenObject != null;
-      }
-  
 }
---
+++ Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class ContainerCounter : BaseCounter
+{
+    public event EventHandler OnPlayerGrabbedObject;
+
+    [SerializeField] private KitchenObjectsSO kitchenObjectsSO;
+
+    public override void Interact(Player player)
+    {
+
+        if (!player.HasKitchenObject())
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectsSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
