be4c26534076fe4abdac0f6fe4390eafb562a5f0
Joseph Antonio Carita <joseph.cmvd@gmail.com>
2025-12-04 12:49:38 -0500

    feat: compiled for web

--- UnitySource/Assets/Scripts/UI/GameOverUI.cs
+++ UnitySource/Assets/Scripts/UI/GameOverUI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +7,7 @@ using UnityEngine.UI;
 public class GameOverUI : MonoBehaviour
 {
       [SerializeField] private TextMeshProUGUI recipesDeliveredText;
+      [SerializeField] private TextMeshProUGUI pointsText;
       [SerializeField] private Button menuButton;
 
   private void Awake()
@@ -24,11 +26,11 @@ public class GameOverUI : MonoBehaviour
 
     private void KitchenGameManager_OnStateChanged(object sender, EventArgs e)
     {
-        Debug.Log(KitchenGameManager.Instance.IsGameOver()); 
         if (KitchenGameManager.Instance.IsGameOver())
         {
             Show();
              recipesDeliveredText.text = DeliveryManager.Instance.GetCompleteRecipeSOList().Count.ToString();
+             pointsText.text = DeliveryManager.Instance.GetCompleteRecipeSOList().Sum(v => v.points).ToString();
         }
         else
         {
